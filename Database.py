716ec0d888c43143d1d0d0576bca965fc28102ac
M.Huizing <M.Huizing@LT-5CG2136LN5>
2023-01-18 10:53:06 +0100

    almost all database code done, error with cursor

--- Database.py
+++ Database.py
@@ -1,11 +1,8 @@
-from email.policy import default
-from turtle import width
-from unittest.mock import DEFAULT
 import mysql.connector 
 from mysql.connector.constants import ClientFlag
 
 class Database:
-    def __init__(self):
+    def _init_(self):
         config = {
             'user': 'u1656_BotTzlvfQc',
             'password': '8J9NeEVFXAIEY+2Jn+^8@sWn',
@@ -22,35 +19,43 @@ class Database:
             print("succes")
 
     def insertDeviceData(self, listDeviceData):
+        #SQL Querrie to check if primary key already exists in the table
+        check_query = "SELECT device_eui FROM device WHERE device_eui = %s"
+        value = (listDeviceData[0],)
+        self.cursor.execute(check_query, value)
+        result = self.cursor.fetchone()
+        if result:
+            print("Primary key already exists in the table")
+            return
         #SQL Querrie to insert data in database
-        sqlDevice = "INSERT INTO device (device_eui,device_id) VALUES (?,?)"
-        valDevice = (listDeviceData[0],listDeviceData[1])
-        self.cursor.execute(sqlDevice, valDevice)
+        query = "INSERT INTO device (device_eui, device_id) VALUES (%s, %s)"
+        value = (listDeviceData[0], listDeviceData[1])
+        self.cursor.execute(query, value)
         self.conn.commit()
 
-    def insertMetaDataPy(self, Light, Pressure, datetime1, device_eui, Temperature):
+    def insertMetaDataPy(self, device_eui, Light, Pressure, datetime1, Temperature):
         #SQL Querrie to insert data in database
-        sql = "INSERT INTO wheater_forecast (device_eui,date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (?,?,?,DEFAULT,?,DEFAULT,?)"
-        val = (device_eui,datetime1,Temperature,default ,Light,default,Pressure)
+        sql = "INSERT INTO wheater_forecast (device_eui, date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (%s,%s,%s,DEFAULT,%s,DEFAULT,%s)"
+        val = (device_eui, datetime1,Temperature,Light,Pressure)
         self.cursor.execute(sql, val)
         self.conn.commit()
 
     def insertMetaDataLhtSaxion(self, datetime1,device_eui,Humidity,Temp_Out,Temperature):
         #SQL Querrie to insert data in database
-        sql = "INSERT INTO wheater_forecast (device_eui,date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (?,?,?,?,DEFAULT,?,DEFAULT)"
-        val = (device_eui,datetime1,Temperature,Temp_Out, default,Humidity, default)
+        sql = "INSERT INTO wheater_forecast (device_eui,date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (%s,%s,%s,%s,DEFAULT,%s,DEFAULT)"
+        val = (device_eui,datetime1,Temperature,Temp_Out,Humidity)
         self.cursor.execute(sql, val)
         self.conn.commit()
 
     def insertMetaDataElse(self, Light,datetime1,device_eui,Humidity,Temperature):
         #SQL Querrie to insert data in database
-        sql = "INSERT INTO wheater_forecast (device_eui,date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (?,?,?,DEFAULT,?,?,DEFAULT)"
-        val = (device_eui,datetime1,Temperature, default,Light,Humidity, default)
+        sql = "INSERT INTO wheater_forecast (device_eui,date_time,temperature,temperature_inside,ambient_light,humidity,barometric_pressure) VALUES (%s,%s,%s,DEFAULT,%s,%s,DEFAULT)"
+        val = (device_eui,datetime1,Temperature,Light,Humidity)
         self.cursor.execute(sql, val)
         self.conn.commit()
 
     def insertDevData(self, latitude, longitude, AirTime,device_eui,datetime1,RSSI):
-        sqlDevMeta = "INSERT INTO device_metadata (latitude, longitude, rssi, air_time,received_at,device_eui) VALUES (?,?,?,?,?,?)"
+        sqlDevMeta = "INSERT INTO device_metadata (latitude, longitude, rssi, air_time,received_at,device_eui) VALUES (%s,%s,%s,%s,%s,%s)"
         valDevMeta = (latitude, longitude,RSSI ,AirTime,datetime1,device_eui)
         self.cursor.execute(sqlDevMeta, valDevMeta)
         self.conn.commit()
