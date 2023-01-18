6345b70adfbc3fb4b8ae38dd286e9cdb2f8f7d66
M.Huizing <M.Huizing@LT-5CG2136LN5>
2023-01-18 11:05:09 +0100

    _init_ to __init__

--- Database.py
+++ Database.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 class Database:
-    def _init_(self):
+    def __init__(self):
         config = {
             'user': 'u1656_BotTzlvfQc',
             'password': '8J9NeEVFXAIEY+2Jn+^8@sWn',
